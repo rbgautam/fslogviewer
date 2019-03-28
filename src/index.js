aec84e143627d30eb3904d6bfab7aeb8fd152d5e
Rahul Gautam <RGautam@iaai.com>
2019-03-28 12:56:11 -0500

    Modified to read file difference and num of lines

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import fs from 'fs'
 import chalk from 'chalk'
-//console.log('Hello fslogger');
+import readline from 'readline'
 
 //Get File list fomr directory
 let logDirPath = '\\\\iaai.com\\EnterpriseServices\\IAATow\\QA\\IAATowMobilityWebService\\';
 //Pick the latest File
 
-let prevFileName = 'Application_IAATow_MobilityService-636885977630999551.log'; 
+let prevFileName = 'Application_IAATow_MobilityService-636888670580483456.log';//'Application_IAATow_MobilityService-636885977630999551.log'; 
 let currFileName = '';
 let prevFileLineCount = 0;
 let currFileLineCount = 0;
 
-fs.readFileSync(logDirPath+prevFileName,'utf-8');
 
-//const testFolder = './tests/';
-let fileList= [];
-fs.readdir(logDirPath, (err, files) => {
-  files.forEach(file => {
-     fileList.push(file);
-  });
-  currFileName = getNewestFile(fileList,logDirPath);
-  currFileLineCount = ReadFileLength(logDirPath+currFileName);
-  DisplayLogContent();
-});
+
+
+const ReadDirlist = () =>{
+    let fileList= [];
+    fs.readdir(logDirPath, (err, files) => {
+    files.forEach(file => {
+        fileList.push(file);
+    });
+    currFileName = getNewestFile(fileList,logDirPath);
+    DisplayLogContent();
+    });
+}
 
 //Store latest file name and # of lines
 function getNewestFile(files, path) {
@@ -49,7 +50,10 @@ const ReadFileLength = (filePath) =>{
         if (chunk[i] == 10) count++;
     })
     .on('end', function() {
-        console.log(count);
+       // console.log(count);
+        currFileLineCount = count;
+        //readLinesFromLog();
+        ReadFileLines();
         return count;
     });
 
@@ -57,13 +61,13 @@ const ReadFileLength = (filePath) =>{
 
 
 const DisplayLogContent = ()=>{
-    console.log(currFileName);
+    console.log( chalk.bgCyan(`Log file: ${currFileName}`));
     //if Lastest file not same as the prevfileName then read whole file
     if(prevFileName !== currFileName){
         readFullLog();
     }else{
         //Else if Latestfilename equals prevFileName then  currcount - lastcount
-        readLinesFromLog();
+        ReadFileLength(logDirPath+currFileName);
     }
     
     
@@ -79,6 +83,62 @@ const readFullLog = () => {
     });
 };
 
-const readLinesFromLog = ()=>{
+const  readLinesFromLog = async ()=>{
+    console.log(currFileLineCount);
+    let linesToRead = currFileLineCount - prevFileLineCount;
+    linesToRead = 10;
     console.log('Lines from log');
+    
+    const fileStream = fs.createReadStream(logDirPath+currFileName);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    //Note: we use the crlfDelay option to recognize all instances of CR LF
+    //('\r\n') in input.txt as a single line break.
+    let lineCount  = 0;
+    rl.on('line', (line) => {
+        lineCount++; 
+        console.log( chalk.redBright(`${line}`));
+        if(lineCount === linesToRead){
+            rl.close();
+            
+        }
+    
+    });
+
+    rl.on('close', function() {
+        //console.log(wantedLines);
+        process.exit(0)
+    });
+
 };
+
+const ReadFileLines = ()=>{
+    fs.readFile(logDirPath+currFileName, "utf8", (err, data) => {
+        if(err){
+            console.log(err);
+        }else{
+            data = data.split("\r\n"); // split the document into lines
+            let linesToRead = currFileLineCount - prevFileLineCount;
+            linesToRead = 10;
+            data.length = 10;    // set the total number of lines to 10
+            //TODO: Read last # of lines
+            console.log(data); //Array containing the 10 lines
+        }
+      });
+}
+
+//let logReadTimer = setInterval(ReadDirlist,2000);
+let timerCount = 0;
+function intervalFunc() {
+    timerCount++;
+    ReadDirlist();
+    if (timerCount == '5') {
+      clearInterval(this);
+    }
+}
+setImmediate(intervalFunc);
+setInterval(intervalFunc, 2000);
+
